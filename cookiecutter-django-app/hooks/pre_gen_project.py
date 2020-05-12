fe3f6897ba4f2b117e8e1065400eac32deeb3516
jinder1s <msingh@edx.org>
2020-05-12 14:02:58 -0400

    Added edx-lint to each cookiecutter

---
+++ cookiecutter-django-app/hooks/pre_gen_project.py
@@ -0,0 +1,17 @@
+"""Pre-generation cookiecutter hook for validation of the app_name parameter."""
+
+import logging
+import re
+import sys
+
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger('pre_gen_project')  # pylint: disable=C0103
+
+APP_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+
+APP_NAME = '{{cookiecutter.app_name}}'
+
+if not re.match(APP_REGEX, APP_NAME):
+    logger.error('Invalid value for app_name "{}"'.format(APP_NAME))
+    sys.exit(1)
