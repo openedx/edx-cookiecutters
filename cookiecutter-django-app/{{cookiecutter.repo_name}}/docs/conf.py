c6f4f5511b4fdfb79410ac9a0eac25fff7ef962d
Brian Mesick <bmesick@edx.org>
2020-06-19 15:19:47 -0400

    Make variable names more inclusive

--- cookiecutter-django-app/{{cookiecutter.repo_name}}/docs/conf.py
+++ cookiecutter-django-app/{{cookiecutter.repo_name}}/docs/conf.py
@@ -88,8 +88,8 @@ source_suffix = '.rst'
 #
 # source_encoding = 'utf-8-sig'
 
-# The master toctree document.
-master_doc = 'index'
+# The top level toctree document.
+top_level_doc = 'index'
 
 # General information about the project.
 project = '{{ cookiecutter.project_name }}'
@@ -306,7 +306,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_target = '{project}.tex'.format(project=project)
 latex_documents = [
-    (master_doc, latex_target, documentation_title,
+    (top_level_doc, latex_target, documentation_title,
      author, 'manual'),
 ]
 
@@ -348,7 +348,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, project_title, documentation_title,
+    (top_level_doc, project_title, documentation_title,
      [author], 1)
 ]
 
@@ -363,7 +363,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, project_title, documentation_title,
+    (top_level_doc, project_title, documentation_title,
      author, project_title, '{{ cookiecutter.project_short_description }}',
      'Miscellaneous'),
 ]
--- python-template/{{cookiecutter.placeholder_repo_name}}/docs/index.rst
+++ python-template/{{cookiecutter.placeholder_repo_name}}/docs/index.rst
@@ -1,4 +1,4 @@
-.. {{ cookiecutter.project_name }} documentation master file, created by
+.. {{ cookiecutter.project_name }} documentation top level file, created by
    sphinx-quickstart on {% now 'local', '%a %b %d %H:%M:%S %Y' %}.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
