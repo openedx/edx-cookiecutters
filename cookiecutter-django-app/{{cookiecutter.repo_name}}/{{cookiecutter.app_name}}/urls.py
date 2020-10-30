4d964dc7201fdb7c9016e394d1979cc44eaa85ea
Tim McCormack <tmccormack@edx.org>
2020-10-30 20:37:57 +0000

    ARCHBOM-1570: Don't fill in a sample url pattern for Django apps
    Just make it a suggestion in a comment, otherwise this can cause
    trouble if the app is integrated before the `urlpatterns` list
    is customized. (In particular, the `r''` default was overriding
    all URLs in the IDA.)

--- cookiecutter-django-app/{{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
+++ cookiecutter-django-app/{{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
@@ -1,9 +1,10 @@
 """
 URLs for {{ cookiecutter.app_name }}.
 """
-from django.conf.urls import url
-from django.views.generic import TemplateView
+from django.conf.urls import url  # pylint disable=unused-import
+from django.views.generic import TemplateView  # pylint disable=unused-import
 
 urlpatterns = [
-    url(r'', TemplateView.as_view(template_name="{{ cookiecutter.app_name }}/base.html")),
+    # TODO: Fill in URL patterns and views here.
+    # url(r'', TemplateView.as_view(template_name="{{ cookiecutter.app_name }}/base.html")),
 ]
