c3129191ab2ddfe1d774d30db4dc4f3862ecc026
Tim McCormack <tmccormack@edx.org>
2020-11-02 21:10:40 +0000

    Fix pylint disabling

--- cookiecutter-django-app/{{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
+++ cookiecutter-django-app/{{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
@@ -1,8 +1,8 @@
 """
 URLs for {{ cookiecutter.app_name }}.
 """
-from django.conf.urls import url  # pylint disable=unused-import
-from django.views.generic import TemplateView  # pylint disable=unused-import
+from django.conf.urls import url  # pylint: disable=unused-import
+from django.views.generic import TemplateView  # pylint: disable=unused-import
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
