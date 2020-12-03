f3e1d34db6e0866d7d8fef13d5d4e65b76cf8f68
jinder1s <msingh@edx.org>
2020-12-03 15:42:56 -0500

    Adding pathlib

--- utils_edx_cookiecutters/layered_cookiecutter.py
+++ utils_edx_cookiecutters/layered_cookiecutter.py
@@ -1,8 +1,9 @@
 """
     Helps create a cookiecutter using layered approach
 """
-import os
-import shutil
+from os import getenv
+from pathlib import Path
+from shutil import shutil_move, shutil_rmtree
 
 from cookiecutter.main import cookiecutter
 from edx_lint.cmd.write import write_main
@@ -16,38 +17,40 @@ class LayeredCookiecutter():
     # cookiecutter can import a template from either github or from a location on local disk.
     # If someone is debugging this repository locally, the below block is necessary to pull in
     #   local versions of the templates
-    EDX_COOKIECUTTER_ROOTDIR = os.getenv('EDX_COOKIECUTTER_ROOTDIR') or 'https://github.com/edx/edx-cookiecutters.git'
+    EDX_COOKIECUTTER_ROOTDIR = Path(getenv('EDX_COOKIECUTTER_ROOTDIR') or 'https://github.com/edx/edx-cookiecutters.git')
 
     def __init__(self, project_rootdir, edx_cookiecutter_rootdir=None):
         if edx_cookiecutter_rootdir is not None:
-            self.EDX_COOKIECUTTER_ROOTDIR = edx_cookiecutter_rootdir
+            self.EDX_COOKIECUTTER_ROOTDIR = Path(edx_cookiecutter_rootdir)
         self.templates = []
-        self.project_rootdir = project_rootdir
+        self.project_rootdir = Path(project_rootdir)
 
     def move(self, src, dest):
         """
         Use to move files or folders without replacement.
         """
-        if os.path.isfile(dest):
-            os.remove(src)
+        src = Path(src)
+        dest = Path(dest)
+        if dest.is_file(dest):
+            src.unlink()
             return
-        if os.path.isdir(src) and os.path.isdir(dest):
-            dir_contents = os.listdir(src)
-            for content in dir_contents:
-                self.move(os.path.join(src, content), os.path.join(dest, content))
-            os.rmdir(src)
+        if src.is_dir() and dest.is_dir():
+            for content in src.iterdir():
+                self.move(src / content, dest / content)
+            src.rmdir()
         else:
-            shutil.move(src, dest)
+            shutil_move(src, dest)
 
     def remove(self, path):
         """
         Use to remove either a file or a whole directory.
         """
-        full_path = os.path.join(self.project_rootdir, path)
-        if os.path.isfile(full_path):
-            os.remove(full_path)
-        elif os.path.isdir(full_path):
-            shutil.rmtree(full_path)
+        path = Path(path)
+        full_path = self.project_rootdir / path
+        if full_path.is_file():
+            full_path.remove()
+        elif full_path.is_dir():
+            shutil_rmtree(full_path)
         else:
             print("{path} not in cookiecutter output".format(path=full_path))
 
@@ -76,16 +79,12 @@ class LayeredCookiecutter():
                     self.EDX_COOKIECUTTER_ROOTDIR,
                     extra_context=template["extra_context"],
                     no_input=True,
-                    directory=template["template_name"],
+                    directory=template["template_name"], 
                 )
 
-            template_output_loc = os.path.join(
-                                    self.project_rootdir,
-                                    template["extra_context"]["placeholder_repo_name"]
-                                    )
-            template_files = os.listdir(template_output_loc)
-            for f in template_files:
-                self.move(os.path.join(template_output_loc, f), os.path.join(self.project_rootdir, f))
+            template_output_loc = self.project_rootdir / Path(template["extra_context"]["placeholder_repo_name"])
+            for f in template_output_loc.iterdir():
+                self.move(template_output_loc / f, self.project_rootdir / f)
 
             for object_name in template["remove_object"]:
                 self.remove(object_name)
