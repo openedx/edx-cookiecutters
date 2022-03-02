48fcac1ff93f8be938ebe6ff609d7f2ee2ca3b51
Ghassan Maslamani <ghassan.maslamani@gmail.com>
2022-03-02 19:37:02 +0200

    feat: replace depreaction use url to re_path also include
     - django has depcreated the use of include and url from
     - django.conf.urls and instead to use re_path and include from
     - django.urls
    
    Co-authored-by: Tim McCormack <tmccormack@edx.org>

--- cookiecutter-django-app/{{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
+++ cookiecutter-django-app/{{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
@@ -1,10 +1,10 @@
 """
 URLs for {{ cookiecutter.app_name }}.
 """
-from django.conf.urls import url  # pylint: disable=unused-import
+from django.urls import re_path  # pylint: disable=unused-import
 from django.views.generic import TemplateView  # pylint: disable=unused-import
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
-    # url(r'', TemplateView.as_view(template_name="{{ cookiecutter.app_name }}/base.html")),
+    # re_path(r'', TemplateView.as_view(template_name="{{ cookiecutter.app_name }}/base.html")),
 ]
