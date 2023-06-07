31b9e5633afd189d84d80627cd2f7c3b7c555c70
Ned Batchelder <ned@edx.org>
2023-06-07 13:15:09 -0400

    refactor: simplify the code in venv.py

--- tests/venv.py
+++ tests/venv.py
@@ -9,17 +9,25 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 
+def _use_virtualenv(venv_path, shell_script):
+    """Run `shell_script` in the already-made virtualenv at `venv_path`."""
+    subprocess.check_call(f'. {venv_path}/bin/activate; {shell_script}', env={}, shell=True)
+
+
 def run_in_virtualenv(shell_script):
     """
-    Set up virtualenv in current directory and run provided shell script
-    with virtualenv active. Virtualenv is deleted after script runs.
+    Create a virtualenv and run the provided shell script in it.
+
+    The virtualenv is created in the current directory and deleted after
+    the script runs.
     """
     with TemporaryDirectory() as parent_dir:
+        pip_txt = shutil.move("requirements/pip.txt", parent_dir)
         venv_path = str(Path(parent_dir) / 'venv')
         try:
             subprocess.check_call(['virtualenv', '-p', sys.executable, '--clear', venv_path])
-            subprocess.check_call(f'. {venv_path}/bin/activate; cp ./requirements/pip.txt {str(Path(parent_dir))}; pip install -qr {str(Path(parent_dir) / "pip.txt")}', env={}, shell=True)
-            subprocess.check_call(f'. {venv_path}/bin/activate; {shell_script}', env={}, shell=True)
+            _use_virtualenv(venv_path, f'pip install -qr {pip_txt}')
+            _use_virtualenv(venv_path, shell_script)
         finally:
             if shutil.rmtree.avoids_symlink_attacks:
                 shutil.rmtree(venv_path, ignore_errors=True)
