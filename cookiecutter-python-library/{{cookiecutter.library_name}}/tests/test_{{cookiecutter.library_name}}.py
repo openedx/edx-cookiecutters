403b620e5a5253714e6e27a7133977ebcd6b7bdd
Tim McCormack <tmccormack@edx.org>
2023-06-13 20:31:39 +0000

    fixup! Add a placeholder test so pytest will succeed for python-library

--- cookiecutter-python-library/{{cookiecutter.library_name}}/tests/test_{{cookiecutter.library_name}}.py
+++ cookiecutter-python-library/{{cookiecutter.library_name}}/tests/test_{{cookiecutter.library_name}}.py
@@ -1,3 +1,11 @@
 """
 Tests for {{cookiecutter.library_name}}.py.
 """
+
+
+def test_placeholder():
+    """
+    Placeholder to allow pytest to succeed before real tests are in place.
+
+    (If there are no tests, it will exit with code 5.)
+    """
