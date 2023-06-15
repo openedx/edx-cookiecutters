b1af90a186c7910406452372bb04ea99bf83087a
Tim McCormack <tmccormack@edx.org>
2023-06-15 20:54:26 +0000

    fixup! Unbreak the for-loop in the model tests
    OK, so that's why the whitespace was managed that way...

--- cookiecutter-django-app/{{cookiecutter.repo_name}}/tests/test_models.py
+++ cookiecutter-django-app/{{cookiecutter.repo_name}}/tests/test_models.py
@@ -4,22 +4,24 @@ Tests for the `{{ cookiecutter.repo_name }}` models module.
 """
 
 import pytest
+{%- if cookiecutter.models != "Comma-separated list of models" -%}
+{%- for model in cookiecutter.models.replace(' ', '').split(',') %}
 
 
-{% if cookiecutter.models != "Comma-separated list of models" -%}
-{%- for model in cookiecutter.models.replace(' ', '').split(',') -%}
 class Test{{ model }}:
     """
     Tests of the {{ model }} model.
     """
 
     @pytest.mark.skip(reason="Placeholder to allow pytest to succeed before real tests are in place.")
-    def test_something(self):
+    def test_placeholder():
         """
         TODO: Delete this test once there are real tests.
         """
 {%- endfor -%}
-{%- else -%}
+{%- else %}
+
+
 @pytest.mark.skip(reason="Placeholder to allow pytest to succeed before real tests are in place.")
 def test_placeholder():
     """
