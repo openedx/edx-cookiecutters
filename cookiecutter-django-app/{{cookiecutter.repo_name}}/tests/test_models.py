d74c4ffac3a907d3cefff668b18f4a75dcbff1ec
Tim McCormack <tmccormack@edx.org>
2023-06-15 19:55:39 +0000

    fixup! Use skipped test for better visibility of placeholder
    Also use this in cookiecutter-django-app, which requires a bit of rework
    of the template in order to deduplicate the import. (Getting the right
    number of newlines is a little delicate.)

--- cookiecutter-django-app/{{cookiecutter.repo_name}}/tests/test_models.py
+++ cookiecutter-django-app/{{cookiecutter.repo_name}}/tests/test_models.py
@@ -2,25 +2,27 @@
 """
 Tests for the `{{ cookiecutter.repo_name }}` models module.
 """
-{%- if cookiecutter.models != "Comma-separated list of models" -%}
-{%- for model in cookiecutter.models.replace(' ', '').split(',') %}
+
+import pytest
 
 
+{% if cookiecutter.models != "Comma-separated list of models" -%}
+{%- for model in cookiecutter.models.replace(' ', '').split(',') -%}
 class Test{{ model }}:
     """
     Tests of the {{ model }} model.
     """
 
+    @pytest.mark.skip(reason="Placeholder to allow pytest to succeed before real tests are in place.")
     def test_something(self):
-        """TODO: Write real test cases."""
+        """
+        TODO: Delete this test once there are real tests.
+        """
 {%- endfor -%}
-{%- else %}
-
-
+{%- else -%}
+@pytest.mark.skip(reason="Placeholder to allow pytest to succeed before real tests are in place.")
 def test_placeholder():
     """
-    Placeholder to allow pytest to succeed before real tests are in place.
-
-    (If there are no tests, it will exit with code 5.)
+    TODO: Delete this test once there are real tests.
     """
 {%- endif %}
--- cookiecutter-python-library/{{cookiecutter.library_name}}/tests/test_{{cookiecutter.library_name}}.py
+++ cookiecutter-python-library/{{cookiecutter.library_name}}/tests/test_{{cookiecutter.library_name}}.py
@@ -2,10 +2,11 @@
 Tests for {{cookiecutter.library_name}}.py.
 """
 
+import pytest
 
+
+@pytest.mark.skip(reason="Placeholder to allow pytest to succeed before real tests are in place.")
 def test_placeholder():
     """
-    Placeholder to allow pytest to succeed before real tests are in place.
-
-    (If there are no tests, it will exit with code 5.)
+    TODO: Delete this test once there are real tests.
     """
