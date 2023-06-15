bd9e4b848f155fbaf077b3efa22ba838a3f83654
Tim McCormack <tmccormack@edx.org>
2023-06-15 21:30:37 +0000

    fixup! more unbreaking -- a little too eager with copy/paste

--- cookiecutter-django-app/{{cookiecutter.repo_name}}/tests/test_models.py
+++ cookiecutter-django-app/{{cookiecutter.repo_name}}/tests/test_models.py
@@ -14,7 +14,7 @@ class Test{{ model }}:
     """
 
     @pytest.mark.skip(reason="Placeholder to allow pytest to succeed before real tests are in place.")
-    def test_placeholder():
+    def test_placeholder(self):
         """
         TODO: Delete this test once there are real tests.
         """
