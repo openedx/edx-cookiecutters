911c3be0d1bd2ee12ffe3e47e8fe28b173a98c3f
Ned Batchelder <ned@edx.org>
2023-08-06 12:36:48 -0400

    test: make github_org_is_right more sophisticated

--- tests/common_tests.py
+++ tests/common_tests.py
@@ -19,8 +19,24 @@ from .venv import all_files
 
 def test_github_org_is_right(options_baked):
     """Make sure no one hard-coded openedx as the GitHub organization."""
-    wrong_github = f"github.com/openedx/{options_baked['repo_name']}"
+    org = options_baked["github_org"]
+    repo = options_baked["repo_name"]
+
+    right_github = f"github.com/{org}/{repo}"
+    if org != "openedx":
+        wrong_github = f"github.com/openedx/{repo}"
+    else:
+        wrong_github = None
+
+    rights = 0
+    wrongs = 0
     for name in all_files():
         with open(name) as f:
             for line in f:
-                assert wrong_github not in line
+                if right_github in line:
+                    rights += 1
+                if wrong_github is not None:
+                    if wrong_github in line:
+                        wrongs += 1
+    assert rights > 1
+    assert wrongs == 0
