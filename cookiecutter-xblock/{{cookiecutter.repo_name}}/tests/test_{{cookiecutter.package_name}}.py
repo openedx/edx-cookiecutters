c082a0bda51569a717ac629f210bc1862a5edf5b
Rebecca Graber <rgraber@2u.com>
2023-12-15 15:37:32 -0500

    fixup!: more quality

--- cookiecutter-xblock/{{cookiecutter.repo_name}}/tests/test_{{cookiecutter.package_name}}.py
+++ cookiecutter-xblock/{{cookiecutter.repo_name}}/tests/test_{{cookiecutter.package_name}}.py
@@ -7,11 +7,12 @@ from xblock.fields import ScopeIds
 from xblock.test.toy_runtime import ToyRuntime
 from my_xblock import MyXBlock
 
+
 class Test{{cookiecutter.class_name}}(TestCase):
     """Tests for {{cookiecutter.class_name}}"""
     def test_my_student_view(self):
         """Test the basic view loads."""
-        scope_ids = ScopeIds('1','2','3','4')
+        scope_ids = ScopeIds('1', '2', '3', '4')
         block = MyXBlock(ToyRuntime(), scope_ids=scope_ids)
         frag = block.student_view()
         as_dict = frag.to_dict()
